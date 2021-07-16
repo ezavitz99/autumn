6baf019301c62b2ce5f954a5a96d60e1c84a032b
ezavitz99 <85895865+ezavitz99@users.noreply.github.com>
2021-07-16 09:15:36 -0700

    Hierarchy Update

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@
 //
 // app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
 // is loaded, and before body.html is loaded.
-import {depthkitComponent} from './components.js'
+import {depthkitComponent} from '../files/components.js'
 AFRAME.registerComponent('depthkit', depthkitComponent)
 import './index.css'
